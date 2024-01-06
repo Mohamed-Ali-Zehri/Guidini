56c2b642fd1befff79160282e2ce507f844c0218
medd7 <medd7517@gmail.com>
2024-01-06 11:27:12 +0100

    touristic application

--- lib/MapPage.dart
+++ lib/MapPage.dart
@@ -1,107 +1,169 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:guidini_app/WelcomePage.dart';
-import 'package:guidini_app/ChatPage.dart';
-
 
 class MapPage extends StatefulWidget {
   @override
-  _MapSearchPageState createState() => _MapSearchPageState();
+  _MapPageState createState() => _MapPageState();
 }
 
-class _MapSearchPageState extends State<MapPage> {
-  GoogleMapController? mapController;
-  TextEditingController searchController = TextEditingController();
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _searchController = TextEditingController();
+  Set<Marker> _markers = {};
+  MapType _currentMapType = MapType.normal;
 
-  @override
-  void dispose() {
-    mapController?.dispose();
-    searchController.dispose();
-    super.dispose();
-  }
+  static final CameraPosition _initialCameraPosition = CameraPosition(
+    target: LatLng(36.8065, 10.1815),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Map Search Page'),
-      ),
+      appBar: AppBar(title: Text('Map App')),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: 'Search places',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    // Handle search action
-                    String query = searchController.text;
-                    // Perform search using the query
-                    // Update the map markers or perform any other action
-                  },
-                ),
-              ),
-            ),
-          ),
+          _searchBar(),
           Expanded(
             child: GoogleMap(
-              onMapCreated: (controller) {
-                setState(() {
-                  mapController = controller;
-                });
+              mapType: _currentMapType,
+              markers: _markers,
+              initialCameraPosition: _initialCameraPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
               },
-              initialCameraPosition: CameraPosition(
-                target: LatLng(33.8869, 9.5375),
-                zoom: 7.0,
-              ),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
             ),
           ),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => WelcomePage()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.map),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MapPage()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.maps_ugc_outlined),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChatPage(),),);
-
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.accessibility),
-              onPressed: () {
-
-              },
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleMapType,
+        tooltip: 'Toggle Map Type',
+        child: Icon(Icons.layers),
       ),
     );
+  }
+
+  Widget _searchBar() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            controller: _searchController,
+            textCapitalization: TextCapitalization.words,
+            decoration: InputDecoration(hintText: 'Search by City'),
+            onFieldSubmitted: (value) => _onSearchSubmitted(value),
+          ),
+        ),
+        IconButton(
+          onPressed: () => _onSearchSubmitted(_searchController.text),
+          icon: Icon(Icons.search),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _onSearchSubmitted(String value) async {
+    try {
+      var placeDetails = await LocationServices().getPlace(value);
+      if (placeDetails.isNotEmpty) {
+        _updateMapLocation(placeDetails);
+      } else {
+        print("No results found or there was an error");
+      }
+    } catch (e) {
+      print('Error fetching place data: $e');
+    }
+  }
+
+  void _updateMapLocation(Map<String, dynamic> placeDetails) {
+    final double lat = placeDetails['geometry']['location']['lat'];
+    final double lng = placeDetails['geometry']['location']['lng'];
+    final LatLng newLocation = LatLng(lat, lng);
+
+    setState(() {
+      _markers.clear();
+      _markers.add(
+        Marker(
+          markerId: MarkerId(newLocation.toString()),
+          position: newLocation,
+          infoWindow: InfoWindow(
+              title: placeDetails['name'], snippet: placeDetails['formatted_address']),
+        ),
+      );
+
+      _controller.future.then((controller) => controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: newLocation, zoom: 15),
+        ),
+      ));
+    });
+  }
+
+  void _toggleMapType() {
+    setState(() {
+      _currentMapType =
+      _currentMapType == MapType.normal ? MapType.satellite : MapType.normal;
+    });
+  }
+}
+
+class LocationServices {
+  final String key =
+      'YOUR_GOOGLE_MAPS_API_KEY'; // Replace with your actual API key
+
+  // Utility method for API call
+  Future<dynamic> _getApiResponse(String url) async {
+    try {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return convert.jsonDecode(response.body);
+      } else {
+        print('Server error: ${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      print('Error occurred: $e');
+      return null;
+    }
+  }
+
+  Future<String> getPlaceId(String input) async {
+    final String encodedInput = Uri.encodeComponent(input);
+    final String url =
+        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$encodedInput&inputtype=textquery&key=$key';
+
+    var json = await _getApiResponse(url);
+    if (json != null) {
+      var placeId = json['candidates'][0]['place_id'] as String;
+      print(placeId);
+      return placeId;
+    } else {
+      return 'Error';
+    }
+  }
+
+  Future<Map<String, dynamic>> getPlace(String input) async {
+    final placeId = await getPlaceId(input);
+    if (placeId == 'Error') {
+      return {};
+    }
+
+    final String url =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
+
+    var json = await _getApiResponse(url);
+    if (json != null) {
+      var results = json['result'] as Map<String, dynamic>;
+      print(results);
+      return results;
+    } else {
+      return {};
+    }
   }
 }
