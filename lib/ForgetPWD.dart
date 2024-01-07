0c805d3c342bbcfd9957d8ff977c87bc8afc2060
medd7 <medd7517@gmail.com>
2024-01-07 19:38:09 +0100

    touristic application

--- lib/ForgetPWD.dart
+++ lib/ForgetPWD.dart
@@ -1,130 +1,113 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:guidini_app/LoginPage.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  TextEditingController _emailController = TextEditingController();
+  Color boxColor = Colors.grey[300]!;
+  Color buttonColor = Colors.blue;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('Password Reset Link Sent! Check Your mail'),
+          );
+        },
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e.message.toString()),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double inputWidth = MediaQuery.of(context).size.width * 0.8;
-    double buttonWidth = inputWidth * 0.8;
-
     return Scaffold(
-      backgroundColor: Colors.grey[350],
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: screenHeight * 0.25),
-          Container(
-            alignment: Alignment.topCenter,
-            width: inputWidth,
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              'RESET PASSWORD',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: HSLColor.fromAHSL(1, 218, 0.75, 0.31).toColor(),
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                fontFamily: 'YourSpecialFont',
-                shadows: [
-                  Shadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(0, 2),
-                    blurRadius: 4,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 120.0, // Adjust the height as needed
+                  child: Image.asset(
+                    'images/logo.png',
                   ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: screenHeight * 0.1),
-          Container(
-            width: inputWidth,
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Email Address',
-                hintStyle: TextStyle(color: Colors.black.withOpacity(0.4)),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.cyan),
                 ),
               ),
-            ),
-          ),
-          Container(
-            width: inputWidth,
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'New Password',
-                hintStyle: TextStyle(color: Colors.black.withOpacity(0.3)),
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.cyan),
-                ),
+              SizedBox(
+                height: 30.0,
               ),
-            ),
-          ),
-          SizedBox(height: 40),
-          Container(
-            width: buttonWidth,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>LoginPage()),);
-                },
-                child: Ink(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: [
-                        HSLColor.fromAHSL(1, 218, 0.75, 0.31).toColor(),
-                        HSLColor.fromAHSL(1, 219, 0.4, 0.52).toColor(),
-                      ],
-                      stops: [0, 0.7],
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  color: boxColor,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.symmetric(vertical: 5),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Confirm',
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white.withOpacity(1),
-                      ),
+                  ],
+                ),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 12.0,
                     ),
                   ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(height: 40),
-          Container(
-            width: buttonWidth,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: ElevatedButton(
-              child: Text(
-                'Cancel',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: HSLColor.fromAHSL(1, 218, 0.75, 0.31).toColor(),
+              SizedBox(height: 16.0),
+              MaterialButton(
+                onPressed: passwordReset,
+                child: Text(
+                  'Reset Password',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                color: buttonColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>LoginPage()),);
-              },
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
