a20b3ec09c99bc2320e366b5c535d4ee9d1a6759
Ilyesksd <ilyes.kasdallah@supcom.tn>
2024-01-07 20:08:57 +0100

    touristic application

--- lib/WelcomePage.dart
+++ lib/WelcomePage.dart
@@ -68,6 +68,7 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SingleChildScrollView(
           child: Container(
